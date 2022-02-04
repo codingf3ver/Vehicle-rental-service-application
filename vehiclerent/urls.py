c4ea0065d5e190f398edd8ad2077aadfc60227ab
Md Tausif <codingf3ver@gmail.com>
2022-02-04 15:47:29 +0530

    update urls.py

--- vehiclerent/urls.py
+++ vehiclerent/urls.py
@@ -2,8 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [ 
-    path('', views.home, name='home'),
-    path('customer/', views.customerCreate, name='customerCreate'),
-    # path('customerlist', views.customerList, name='customerList'),
-    path('rentbooking', views.rentBookingCreate, name='rentBookingCreate'),
+    path('', views.home, name='home'), # home page view
+    path('customer/', views.customerCreate, name='customerCreate'), # register customer info page view
+    path('rentbooking', views.rentBookingCreate, name='rentBookingCreate'), # rent booking page
 ]
