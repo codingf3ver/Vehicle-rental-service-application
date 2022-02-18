2ffd789aa2971f5709a2332b2b8dfe293f53e00a
Md Tausif <codingf3ver@gmail.com>
2022-02-18 18:18:12 +0530

    view updated for restAPI

--- vehiclerent/views.py
+++ vehiclerent/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect, get_object_or_404
 from django.http import HttpResponse
-from vehiclerent.models import Customer ,Inventory, RentBooking # models or table
+from vehiclerent.models import Customer ,Inventory, RentBooking
 from django.contrib import messages
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import InventorySerializer
 
 
-# home page of the app
+
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
-# getting customer information from form as well as storing into database
 def customerCreate(request):
-    
     if request.method == 'POST':
         CustomerName = request.POST['CustomerName']        
         PhoneNumber = request.POST['PhoneNumber']
@@ -18,15 +20,16 @@ def customerCreate(request):
         Address = request.POST['Address']
         Customer.objects.create(CustomerName=CustomerName, PhoneNumber=PhoneNumber, Email=Email, Address=Address)
         messages.success(request, 'Congratulations! customer has successfully been added.')
-          
+        
+        
     return render(request, 'customerCreate.html')
 
-# Booking request from users
+
 def rentBookingCreate(request):
 
     if request.method == 'POST':
 
-        # form data from user
+        # form data
         CustomerName = request.POST['CustomerName']
         VehicleType = request.POST['VehicleType']
         RentDate = request.POST['RentDate']
@@ -43,19 +46,29 @@ def rentBookingCreate(request):
             messages.error(request, f'Return date cannot be before rent date!')
             return render(request, 'rentBookingCreate.html')
 
-        # Vehicle inventory check from database
+        # Vehicle inventory check
         vehicleInventory = Inventory.objects.get(VehicleType=VehicleType)
+        
         if vehicleInventory.VehicleInventory>0:
             vehicleInventory.VehicleInventory -= 1
             vehicleInventory.save()
-            # storing the updated value of inventory 
+            # Rent booking create
             RentBooking.objects.create( CustomerName=CustomerName, VehicleType=VehicleType, RentDate=RentDate, ReturnDate=ReturnDate) 
             # sucess message for vehicle inventory
             messages.success(request, 'Congratulations! Rent booking has successfully been created.')
         
-        # if inventory is out of stock         
         if  vehicleInventory.VehicleInventory < 1:
             messages.error(request, f'Oops! {VehicleType} parking space is full')
             return render(request, 'rentBookingCreate.html')
         
     return render(request, 'rentBookingCreate.html')
+
+
+class InventoryList(APIView):
+    def get(self,request):
+        inventory = Inventory.objects.all()
+        serializer = InventorySerializer(inventory, many=True)
+        return Response(serializer.data)
+    
+    def post(self):
+        pass
